7b0bc2be8e255c42daae414abb78ff16777e0193
Roberto Zalazar <servicios773@gmail.com>
2023-06-06 20:15:10 -0300

    creando componentes

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NabvarComponent } from './components/nabvar/nabvar.component';
+import { CoverComponent } from './components/cover/cover.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ProyectComponent } from './components/proyect/proyect.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillComponent } from './components/skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NabvarComponent,
+    CoverComponent,
+    ExperienceComponent,
+    ProyectComponent,
+    FooterComponent,
+    AboutMeComponent,
+    EducationComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
